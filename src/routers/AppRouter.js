aeeb9e4d8facb76bc6448203eb33b533703429e6
Gabriel <rocamora_gabriel@hotmail.com>
2022-06-12 20:52:13 +0200

    Incorporación de 2º router con componente DashboardRoutes

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { LoginPage } from '../components/login/LoginPage';
+import { DashboardRoutes } from './DashboardRoutes';
+
+export const AppRouter = () => {
+    
+    return (
+        <Router>
+            <div>
+                <Routes>
+                    <Route exact path="/login" element={ <LoginPage /> }></Route>
+                    <Route path="*" element={ <DashboardRoutes /> }></Route>
+                </Routes>
+            </div>
+        </Router>
+    )
+}
---
+++ src/routers/DashboardRoutes.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { Routes, Route, Navigate } from "react-router-dom";
+import { DcPage } from "../components/dc/DcPage";
+import { HeroPage } from "../components/heroes/HeroPage";
+import { MarvelPage } from "../components/marvel/MarvelPage";
+import { Navbar } from "../components/ui/Navbar"
+
+export const DashboardRoutes = () => {
+    
+    return (
+        <>
+            <Navbar />
+
+            <Routes>
+                <Route exact path="/marvel" element={ <MarvelPage /> }></Route>
+                <Route exact path="/dc" element={ <DcPage /> }></Route>
+                <Route exact path="/heroe/:heroId" element={ <HeroPage /> }></Route>
+            
+                <Route path="*" element={ <Navigate to="/marvel" replace /> }></Route>
+            </Routes>
+        </>
+    )
+}
